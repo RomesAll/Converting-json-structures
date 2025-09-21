51ffcc0faa937d6b243658045b4a41abee8f97a5
Roman Beskokotov <roma@gmail.com>
2025-09-21 18:45:43 +0300

    Добавил CORS

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.workers import router as router_workers
 from routers.resumes import router as router_resumes
 import uvicorn
 
 app = FastAPI()
-
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 app.include_router(router=router_workers, prefix='/api/v1/workers', tags=['Сотрудники'])
 app.include_router(router=router_resumes, prefix='/api/v1/resumes', tags=['Резюме'])
 
