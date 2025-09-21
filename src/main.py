3ba41c7f430340cbcbeba3d4150bc5397672e82e
Roman Beskokotov <roma@gmail.com>
2025-09-21 20:24:09 +0300

    Добавил кнопки в html для вызова ручек

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from routers.workers import router as router_workers
-from routers.resumes import router as router_resumes
+from routers.resumes import PaginationParamsDep, router as router_resumes
 import uvicorn
+
+from service import WorkersCRUDService
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 app.include_router(router=router_workers, prefix='/api/v1/workers', tags=['Сотрудники'])
 app.include_router(router=router_resumes, prefix='/api/v1/resumes', tags=['Резюме'])
+
+@app.get('/')
+async def get_greetings(request: Request):
+    return {'Hello' : request.client.host}
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True, host='0.0.0.0', port=8000)
